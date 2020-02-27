494a2b23676fe1f60b03fd3d8562fbac9e5e290f
ijchoi <ijchoi@gmarineservice.com>
2020-02-27 09:32:53 +0900

    2020.02.27

---
+++ ExchangeRate/ExchangeRate/ExchangeRate.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace ExchangeRate
+{
+    partial class ExchangeRate
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 54);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(1438, 335);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(268, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "한국수출입은행 환율 정보";
+            // 
+            // lblDate
+            // 
+            this.lblDate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(1389, 29);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(61, 12);
+            this.lblDate.TabIndex = 2;
+            this.lblDate.Text = "2999.12.31";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(16, 406);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(770, 250);
+            this.webBrowser1.TabIndex = 3;
+            this.webBrowser1.Url = new System.Uri("https://sbiz.wooribank.com/biz/Dream?withyou=FXCNT0007&rc=0&divType=1&lang=KOR", System.UriKind.Absolute);
+            // 
+            // ExchangeRate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1462, 678);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "ExchangeRate";
+            this.Text = "ExchangeRate";
+            this.Load += new System.EventHandler(this.ExchangeRate_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ ExchangeRate/ExchangeRate/ExchangeRate.cs
@@ -0,0 +1,128 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExchangeRate
+{
+    public partial class ExchangeRate : Form
+    {
+        public DateTime gDt = DateTime.Now;
+        //public DateTime gDt = DateTime.Now.AddDays(5);
+
+        public ExchangeRate()
+        {
+            InitializeComponent();
+        }
+
+        private void ExchangeRate_Load(object sender, EventArgs e)
+        {
+
+            SetupDataGridView();
+
+
+            using (System.Net.WebClient wc = new System.Net.WebClient())
+            {
+                wc.Encoding = System.Text.Encoding.UTF8;
+
+
+                //gDt = DateTime.Now.AddDays(-2);
+
+                bool xExe = true;
+                string jsonURL = "";
+                string json = "";
+                JArray array = new JArray();
+                while (xExe)
+                {
+                    jsonURL = "https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=YSpdSDfWSuga85SwTalCACE2j6SGyRZ4&searchdate="
+                                    + gDt.ToString("yyyyMMdd") + "&data=AP01";
+                    json = wc.DownloadString(jsonURL); // API 사이트에서 json 받아옴
+                    array = JArray.Parse(json);
+
+                    if (array.Count() > 0)
+                    {
+                        xExe = false;
+                    }
+                    else
+                    {
+                        gDt = gDt.AddDays(-1);
+                    }
+
+                }
+
+                string[] xRow = new string[10];
+
+                foreach (JObject jobj in array)
+                {
+                    //if (jobj["result"].ToString() == "1")
+                    //{
+                        string xCurUnit = jobj["cur_unit"].ToString();
+                        string xCurNm = jobj["cur_nm"].ToString();
+                        string xTtb = jobj["ttb"].ToString();
+                        string xTts = jobj["tts"].ToString();
+                        string xDealBasR = jobj["deal_bas_r"].ToString();
+                        string xBkpr = jobj["bkpr"].ToString();
+                        string xYyEfeeR = jobj["yy_efee_r"].ToString();
+                        string xTenDdEfeeR = jobj["ten_dd_efee_r"].ToString();
+                        string xKftcDealBasR = jobj["kftc_deal_bas_r"].ToString();
+                        string xKftcBkpr = jobj["kftc_bkpr"].ToString();
+
+                        xRow = new string[] { xCurUnit, xCurNm, xTtb, xTts, xDealBasR, xBkpr, xYyEfeeR, xTenDdEfeeR, xKftcDealBasR, xKftcBkpr };
+
+                        dataGridView1.Rows.Add(xRow);
+                    //}
+
+                    dataGridView1.Columns[0].DisplayIndex = 0;
+                    dataGridView1.Columns[1].DisplayIndex = 1;
+                    dataGridView1.Columns[2].DisplayIndex = 2;
+                    dataGridView1.Columns[3].DisplayIndex = 3;
+                    
+                    //dataGridView1.AutoResizeColumns();
+
+
+                }
+            }
+
+            lblDate.Text = gDt.ToString("yyyy.MM.dd");
+        }
+
+        private void SetupDataGridView()
+        {
+            DataGridView grd = dataGridView1;
+
+            grd.ColumnCount = 10;
+
+            grd.Columns[0].Name = "CUR_UNIT [통화코드]";
+            grd.Columns[1].Name = "CUR_NM [국가/통화명]";
+            grd.Columns[2].Name = "TTB [전신환(송금) - 받으실때]";
+            grd.Columns[3].Name = "TTS [전신환(송금) - 보내실때]";
+            grd.Columns[4].Name = "DEAL_BAS_R [매매 기준율]";
+            grd.Columns[5].Name = "BKPR [장부가격]";
+            grd.Columns[6].Name = "YY_EFEE_R [년환가료율]";
+            grd.Columns[7].Name = "TEN_DD_EFEE_R [10일환가료율]";
+            grd.Columns[8].Name = "KFTC_DEAL_BAS_R [서울외국환중계 - 매매기준율]";
+            grd.Columns[9].Name = "KFTC_BKPR [서울외국환중계 - 장부가격]";
+
+            grd.Columns[0].Width = 120;
+            grd.Columns[1].Width = 140;
+            grd.Columns[2].Width = 140;
+            grd.Columns[3].Width = 140;
+            grd.Columns[4].Width = 140;
+            grd.Columns[5].Width = 120;
+            grd.Columns[6].Width = 140;
+            grd.Columns[7].Width = 140;
+            grd.Columns[8].Width = 140;
+            grd.Columns[9].Width = 140;
+            
+
+
+
+        }
+    }
+}
